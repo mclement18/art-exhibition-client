175f3eef5b5d78bcd5c08b2e0d7ee915ba7e9cfc
Mathieu Clément <mcfly1893@gmail.com>
2022-08-30 19:23:38 +0200

    Setup bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min.js')
+  }, [])
   return <Component {...pageProps} />
 }
 
